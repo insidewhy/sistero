0a76a4d430fa204f2a092392c7c29b705b6e826e
James Pike <jpike@chilon.net>
2016-01-07 21:05:31 +0000

    option parser improvements
     * for_all to set options for all commands
     * short form of #on to parse option directly into config which is now
       returned in a hash from parse/parse! (along with cmd and args)

--- lib/optparse-subcommand.rb
+++ lib/optparse-subcommand.rb
@@ -1,5 +1,27 @@
-class SubcommandOptionParser < OptionParser
+class OptionParserExtra < OptionParser
+  def initialize(config = {})
+    @config = config
+    super
+  end
+
+  # like OptionParser#on but if a block is not supplied the last argument (which
+  # should be a string) is used as a hash key to store that value within @config.
+  def on *opts, &block
+    if block
+      super *opts, &block
+    else
+      key = opts.pop
+      super *opts do |val|
+        @config[key] = val
+      end
+    end
+  end
+end
+
+class SubcommandOptionParser < OptionParserExtra
   def initialize
+    # options applicable to all subcommands
+    @for_all = []
     @subcommands = {}
     super
   end
@@ -8,9 +30,13 @@ class SubcommandOptionParser < OptionParser
     @subcommands[name] = { name: name, banner: banner, parse_cmdline: block }
   end
 
+  # add a block to configure every subcommand
+  def for_all &block
+    @for_all.push block
+  end
+
   def to_s
     ret = super
-
     max_len = @subcommands.values.map { |subcmd| subcmd[:name].length }.max
     ret += "\ncommands:\n"
     @subcommands.values.each do |subcmd|
@@ -18,11 +44,11 @@ class SubcommandOptionParser < OptionParser
       prefix += ' ' * (max_len - prefix.length + 2)
       ret += "    #{prefix}  #{subcmd[:banner]}\n"
     end
-
     ret
   end
 
   def parse!(args = ARGV)
+    apply_for_all self
     order! args
     raise "must supply subcommand" if args.empty?
 
@@ -31,16 +57,22 @@ class SubcommandOptionParser < OptionParser
     raise "invalid subcommand: #{cmd}" unless @subcommands.has_key? cmd
     args.shift
 
-    positionals = OptionParser.new do |subop|
+    positionals = OptionParserExtra.new(@config) do |subop|
+      apply_for_all subop
       subop.banner = subcmd_meta[:banner]
       parse_cmdline = subcmd_meta[:parse_cmdline]
       parse_cmdline.call(subop) if parse_cmdline
     end.order! args
 
-    [cmd, positionals]
+    { cmd: cmd, args: positionals, config: @config }
   end
 
   def parse(args = ARGV)
     parse! args.clone
+  end
+
+ private
+  def apply_for_all op
+    @for_all.each { |block| block.call op } unless @for_all.empty?
   end
 end
